28f1184f0c0da474527ec041d40478b53fa2da56
mjswemmer <michael@agrinous.com>
2021-05-29 14:01:01 +1000

    tiny clean up

---
+++ src/lib/functions.js
@@ -0,0 +1,7 @@
+export function playSound(url) {
+  const audio = new Audio(url);
+  audio.play();
+}
+<button onclick="playSound('https://your-file.mp3');">Play</button>;
+
+// will come in handy ;)
--- src/reducer.js
+++ src/reducer.js
@@ -10,17 +10,14 @@ const initialState = {
 
 export const MainReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.UPDATE_ROUND:
-      return state;
-    case ActionTypes.UPDATE_ROUND_LENGTH:
-      return state;
-    case ActionTypes.UPDATE_REST_LENGTH:
-      return state;
-    case ActionTypes.UPDATE_END_OF_ROUND_COUNT:
-      return state;
-    case ActionTypes.UPDATE_START_ROUND_COUNT:
-      return state;
+    case ActionTypes.SETTINGS:
+      return updateSettings(state);
     default:
       return state;
   }
+};
+
+export const updateSettings = (initialState) => {
+  const { rounds, roundLength, restLength, endOfRoundCount, startCountDown } =
+    initialState;
 };
